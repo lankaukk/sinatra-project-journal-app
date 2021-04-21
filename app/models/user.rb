ec16fcadd3905922f860988738eb23843f5eb0a7
McKayla Lankau <lankaukk@yahoo.com>
2021-04-21 14:56:54 -0400

    added user model

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+    has_secure_password
+    has_many :tweets
+  
+    def slug
+      @slug = username.gsub(" ","-").downcase
+    end
+  
+    def self.find_by_slug(slug)
+        self.all.find {|t| t.slug == slug}
+    end
+  end
